5ac47cf36cd540e1397ad19a585a8dbf6c7c7f3f
Vsevolod Pukhov <pukhov.vyu@phystech.edu>
2020-10-08 01:41:26 +0300

    OpenMP task2

--- OpenMP2/src/main.cpp
+++ OpenMP2/src/main.cpp
@@ -3,9 +3,70 @@
 #include <fstream>
 #include <omp.h>
 
+#include <assert.h>
+struct __attribute__((aligned(64))) double_al {
+	double val;
+};
+
+#define DIV_ROUND_UP(a, b) ({	\
+	typeof((a)) __a = (a);	\
+	typeof((b)) __b = (b);	\
+	(__a + __b - 1) / __b;})
+
+#if 0
+double calc_thread(uint32_t x_beg, uint32_t x_end)
+{
+	double res = 0;
+	for (uint32_t i = x_end - 1; i >= x_beg; --i)
+		res += (1.0 / i);
+	return res;
+}
+#else
+#define N_BATCH 64
+double calc_thread(uint32_t x_beg, uint32_t x_end)
+{
+	uint32_t batch_sz = DIV_ROUND_UP((x_end - x_beg), N_BATCH);
+	double res = 0;
+	uint32_t tbeg = x_beg;
+
+	for (uint32_t b = 0; b < N_BATCH; ++b) {
+		double tres = 0;
+		uint32_t tend = tbeg + batch_sz;
+		tend = (tend > x_end) ? x_end : tend;
+
+		for (uint32_t i = tend - 1; i >= tbeg; --i)
+			tres += (1.0 / i);
+
+		tbeg += batch_sz;
+		res += tres;
+	}
+	return res;
+}
+#endif
+
 double calc(uint32_t x_last, uint32_t num_threads)
 {
-  return 0;
+	double_al *arr = (double_al*) malloc(sizeof(*arr) * num_threads);
+	assert(arr);
+
+	uint32_t batch_sz = DIV_ROUND_UP(x_last, num_threads);
+
+	#pragma omp parallel num_threads(num_threads)
+	{
+		int tid = omp_get_thread_num();
+
+		uint32_t x_beg = 1 + batch_sz * tid;
+		uint32_t x_end = (tid == num_threads - 1) ? 
+			x_last + 1 : x_beg + batch_sz;
+		arr[tid].val = calc_thread(x_beg, x_end);
+	}
+
+	double res = 0;
+	for (uint32_t i = 0; i < num_threads; ++i)
+		res += arr[i].val;
+
+	free(arr);
+	return res;
 }
 
 int main(int argc, char** argv)
