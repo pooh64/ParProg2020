6aedcd9b7ba2d05758b6ce2eb2f657f4cdf06726
Vsevolod Pukhov <pukhov.vyu@phystech.edu>
2020-11-05 21:15:40 +0300

    Loop1 done

--- Loop1/src/main.cpp
+++ Loop1/src/main.cpp
@@ -1,22 +1,65 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#define OMPI_SKIP_MPICXX 1
 #include <mpi.h>
 #include <unistd.h>
 #include <cmath>
+#include <vector>
+
+#define BCAST(ptr, root) MPI_Bcast((ptr), sizeof(*(ptr)), MPI_CHAR, (root), MPI_COMM_WORLD)
+
+#if 0
+#define TRACE(fmt, ...) printf("[%d]: " fmt "\n", rank, ##__VA_ARGS__)
+#else
+#define TRACE(fmt, ...)
+#endif
+
+#define div_roundup(x, y) ({            \
+        typeof(y) __y = y;              \
+        (((x) + (__y - 1)) / __y); })
+
+#define min(x, y) ({                    \
+        typeof(x) __x = (x);            \
+        typeof(y) __y = (y);            \
+        (__x < __y) ? __x : __y;  })
+
 
 void calc(double* arr, uint32_t ySize, uint32_t xSize, int rank, int size)
 {
-  if (rank == 0 && size > 0)
-  {
-    for (uint32_t y = 0; y < ySize; y++)
-    {
-      for (uint32_t x = 0; x < xSize; x++)
-      {
-        arr[y*xSize + x] = sin(0.00001*arr[y*xSize + x]);
-      }
-    }
-  }
+	BCAST(&xSize, 0);
+	BCAST(&ySize, 0);
+	int const len = xSize * ySize;
+	int const task_sz = div_roundup(len, size);
+	std::vector<double> task(task_sz);
+	std::vector<int> offs_len;
+
+	if (!rank) {
+		offs_len.resize(2 * size);
+		for (int i = 0; i < size; ++i) {
+			offs_len[i] = min(task_sz * i, len);
+			offs_len[i+size] = min(task_sz, len - offs_len[i]);
+		}
+	}
+	TRACE("scattering");
+	MPI_Scatterv(arr, &offs_len[size], &offs_len[0], MPI_DOUBLE,
+			&task[0], task_sz, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	int truncated = min(task_sz, len - min(task_sz * rank, len));
+	TRACE("truncated=%d", truncated);
+
+	for (int i = 0; i < truncated; ++i)
+		task[i] = sin(0.00001*task[i]);
+
+	TRACE("gathering");
+	MPI_Gatherv(&task[0], truncated, MPI_DOUBLE,
+			arr, &offs_len[size], &offs_len[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+#if 0
+	uint32_t len = xSize * ySize;
+	for (uint32_t i = 0; i < len; ++i)
+		arr[i] = sin(0.00001*arr[i]);
+#endif
 }
 
 int main(int argc, char** argv)
