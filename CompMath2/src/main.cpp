0bf91071da302cc01e6c6e98f835ca9f12a2d1ef
Vsevolod Pukhov <pukhov.vyu@phystech.edu>
2020-11-27 19:56:10 +0300

    CompMath2: final state

--- CompMath2/src/main.cpp
+++ CompMath2/src/main.cpp
@@ -41,59 +41,7 @@ void calc_iter(double *out, double *in, double *border[2], int y_max, int x_max,
 	}
 }
 
-#if 0
-ARGS____
-void calc(double* frame, uint32_t ySize, uint32_t xSize, double delta, int rank, int size)
-{
-  if (rank == 0 && size > 0)
-  {
-    double diff = 0;
-    double *border[2];
-    double* tmpFrame = new double[ySize * xSize];
-    // Prepare tmpFrame
-    for (uint32_t y = 0; y < ySize; y++)
-    {
-      tmpFrame[y*xSize] = frame[y*xSize];
-      tmpFrame[y*xSize + xSize - 1] = frame[y*xSize + xSize - 1];
-    }
-    for (uint32_t x = 1; x < xSize - 1; x++)
-    {
-      tmpFrame[x] = frame[x];
-      tmpFrame[(ySize - 1)*xSize + x] = frame[(ySize - 1)*xSize + x];
-    }
-    // Calculate first iteration
-    border[0] = &frame[0];
-    border[1] = &frame[xSize*(ySize-1)];
-    calc_iter(&tmpFrame[xSize], &frame[xSize], border, xSize, ySize-2, diff);
-
-    double* currFrame = tmpFrame;
-    double* nextFrame = frame;
-    uint32_t iteration = 1;
-    // Calculate frames
-    while (diff > delta)
-    {
-      diff = 0;
-      border[0] = &currFrame[0];
-      border[1] = &currFrame[xSize*(ySize-1)];
-      calc_iter(&nextFrame[xSize], &currFrame[xSize], border, xSize, ySize-2, diff);
-      std::swap(currFrame, nextFrame);
-      iteration++;
-    }
-
-    // Copy result from tmp
-    if (iteration % 2 == 1)
-    {
-      for (uint32_t i = 0; i < xSize*ySize; i++)
-      {
-        frame[i] = tmpFrame[i];
-      }
-    }
-    delete tmpFrame;
-  }
-}
-#else
-
-void init_border(double *border[2], double *frame, int ySize, int xSize, int rank, int size)
+void init_border(double *border[2], double *frame, int ySize, int y_self, int xSize, int rank, int size)
 {
 	int len = sizeof(*frame) * xSize;
 	if (rank == 0)
@@ -105,39 +53,48 @@ void init_border(double *border[2], double *frame, int ySize, int xSize, int ran
 
 	if (rank == 0)
 		SEND(size-1, &frame[(ySize-1)*xSize], len);
-	if (rank == size - 1)
-		RECV(0, border[1], len);
+	if (rank == size - 1) {
+		if (!y_self)
+			RECV(0, border[0], len);
+		else
+			RECV(0, border[1], len);
+	}
 }
 
-void sync_border(double *border[2], double *buf, int ySize, int xSize, int rank, int size)
+void sync_border(double *border[2], double *buf, int y_self, int xSize, int rank, int size)
 {
 	if (size == 1)
 		return;
 	int len = sizeof(*buf) * xSize;
 
+	if (!y_self) {
+		if (rank != size - 1)
+			RECV(rank+1, border[0], len);
+		SEND(rank-1, border[0], len);
+		
+		RECV(rank-1, border[1], len); // discard
+		if (rank != size - 1)
+			SEND(rank+1, border[1], len); // dummy
+		return;
+	}
+
+	if (rank != 0)
+		SEND(rank-1, &buf[0], len);
+
 	if (rank != size - 1) {
-		if (!ySize)
-			SEND(rank+1, border[1], len);
-		else
-			SEND(rank+1, &buf[xSize*(ySize-1)], len);
-	}
-
-	if (rank != 0) {
-		RECV(rank-1, border[0], len);
-		if (!ySize)
-			SEND(rank-1, border[0], len);
-		else
-			SEND(rank-1, &buf[0], len);
-	}
-
-	if (rank != size - 1)
 		RECV(rank+1, border[1], len);
+		SEND(rank+1, &buf[(y_self-1)*xSize], len);
+	}
+
+	if (rank != 0)
+		RECV(rank-1, border[0], len);
 }
 
 void calc(double *frame, uint32_t ySize, uint32_t xSize, double delta, int rank, int size)
 {
 	BCAST(&xSize, 0);
 	BCAST(&ySize, 0);
+	BCAST(&delta, 0);
 
 	split_buf<double> task(rank, size);
 	split_buf<double> diff(rank, size);
@@ -147,21 +104,19 @@ void calc(double *frame, uint32_t ySize, uint32_t xSize, double delta, int rank,
 	diff.set_each(1);
 	task.split((ySize-2)*xSize, xSize);
 	task.scatter(&frame[xSize]);
-	std::vector<double> tmp;
-	int self_y = task.buf_len / xSize;
-	init_border(border, frame, ySize, xSize, rank, size);
+	std::vector<double> tmp(task.buf);
+	int y_self = task.buf_len / xSize;
+	init_border(border, frame, ySize, y_self, xSize, rank, size);
 
-	tmp = task.buf;
-	tmp.resize(xSize);
 	do {
 		*diff.get() = 0;
-		sync_border(border, &task.buf[0], self_y, xSize, rank, size);
-		calc_iter(&tmp[0], &task.buf[0], border, self_y, xSize, *diff.get());
+		sync_border(border, &task.buf[0], y_self, xSize, rank, size);
+		calc_iter(&tmp[0], &task.buf[0], border, y_self, xSize, *diff.get());
 		task.buf.swap(tmp);
 
 		diff.gather(&diff_scbuf[0]);
 		if (!rank) {
-			double sum = std::accumulate(diff_scbuf.begin(), diff_scbuf.end(), 0);
+			double sum = std::accumulate(diff_scbuf.begin(), diff_scbuf.end(), 0.0);
 			std::fill(diff_scbuf.begin(), diff_scbuf.end(), sum);
 		}
 		diff.scatter(&diff_scbuf[0]);
@@ -169,7 +124,6 @@ void calc(double *frame, uint32_t ySize, uint32_t xSize, double delta, int rank,
 
 	task.gather(&frame[xSize]);
 }
-#endif
 
 int main(int argc, char** argv)
 {
--- split_buf.h
+++ split_buf.h
@@ -4,7 +4,7 @@
 #define BCAST(ptr, root) MPI_Bcast((ptr), sizeof(*(ptr)), MPI_CHAR, (root), MPI_COMM_WORLD)
 
 #define SEND(dest, buf, size) MPI_Send(buf, size, MPI_CHAR, dest, 0, MPI_COMM_WORLD)
-#define RECV(src,  buf, size) MPI_Send(buf, size, MPI_CHAR, src,  0, MPI_COMM_WORLD)
+#define RECV(src,  buf, size) MPI_Recv(buf, size, MPI_CHAR, src,  0, MPI_COMM_WORLD, NULL)
 
 #define div_roundup(x, y) ({            \
         typeof(y) __y = y;              \
